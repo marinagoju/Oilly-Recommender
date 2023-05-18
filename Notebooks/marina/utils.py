2be8ae6d38c0b7074ea9099ac0c9a2de6fa188c6
marinagoju <122385018+marinagoju@users.noreply.github.com>
2023-05-18 17:49:45 +0200

    first commit

---
+++ Notebooks/marina/utils.py
@@ -0,0 +1,37 @@
+## COORDENADAS DEL EPICENTRO DE CADA DISTRITO
+
+distritos = [
+    {"district": "Centro", "latitude": 40.415363, "longitude": -3.707398},
+    {"district": "Arganzuela", "latitude": 40.398068, "longitude": -3.693734},
+    {"district": "Retiro", "latitude": 40.411894, "longitude": -3.676574},
+    {"district": "Salamanca", "latitude": 40.427919, "longitude": -3.680877},
+    {"district": "Chamartín", "latitude": 40.462428, "longitude": -3.676057},
+    {"district": "Tetuán", "latitude": 40.466676, "longitude": -3.698984},
+    {"district": "Chamberí", "latitude": 40.436247, "longitude": -3.703830},
+    {"district": "Fuencarral-El Pardo", "latitude": 40.515923, "longitude": -3.785586},
+    {"district": "Moncloa-Aravaca", "latitude": 40.439883, "longitude": -3.744509},
+    {"district": "Latina", "latitude": 40.388981, "longitude": -3.753525},
+    {"district": "Carabanchel", "latitude": 40.377818, "longitude": -3.751209},
+    {"district": "Usera", "latitude": 40.383553, "longitude": -3.706070},
+    {"district": "Puente de Vallecas", "latitude": 40.389491, "longitude": -3.669062},
+    {"district": "Moratalaz", "latitude": 40.407251, "longitude": -3.644992},
+    {"district": "Ciudad Lineal", "latitude": 40.448431, "longitude": -3.650495},
+    {"district": "Hortaleza", "latitude": 40.474457, "longitude": -3.640892},
+    {"district": "Villaverde", "latitude": 40.345610, "longitude": -3.695956},
+    {"district": "Villa de Vallecas", "latitude": 40.367040, "longitude": -3.612046},
+    {"district": "Vicálvaro", "latitude": 40.396584, "longitude": -3.576622},
+    {"district": "San Blas-Canillejas", "latitude": 40.428918, "longitude": -3.609064},
+    {"district": "Barajas", "latitude": 40.477628, "longitude": -3.579394}
+]
+
+
+'''
+Aída - 1,2,3 - 'Centro', 'Arganzuela', 'Retiro'
+Carlos - 4,5,6 - 'Salamanca', 'Chamartín', 'Tetuán'
+Marina - 7,8,9 - 'Chamberí', 'Fuencarral-El Pardo', 'Moncloa-Aravaca'
+Fernando - 10,11,12 - 'Latina', 'Carabanchel', 'Usera'
+Víctor - 13,14,15 - 'Puente de Vallecas', 'Moratalaz', 'Ciudad Lineal'
+Richi - 16,17,18 -  'Hortaleza', 'Villaverde', 'Villa de Vallecas'
+Álvaro - 19,20,21 - 'Vicálvaro', 'San Blas-Canillejas', 'Barajas'
+
+'''
